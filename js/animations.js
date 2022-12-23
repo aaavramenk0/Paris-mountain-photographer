bef892c84658efa16a457d67a64d401b8427faaf
Aleksandr <alex.avramenko2004@gmail.com>
2022-12-22 22:21:22 -0500

    Adding some animations for objects

---
+++ js/animations.js
@@ -0,0 +1,42 @@
+/* Logo animation*/
+const logo = document.getElementById('logo');
+
+logo.addEventListener("mouseover", () => {
+    logo.classList.add('animate__swing');
+})
+logo.addEventListener('animationend', () => {
+    logo.classList.remove('animate__swing');
+});
+
+
+/* Top navigation bar animation */
+const navbarItem = document.querySelectorAll('.navbar-link');
+
+for (let navbarIndex = 0; navbarIndex < navbarItem.length; navbarIndex++) {
+    navbarItem[navbarIndex].addEventListener("mouseover", () => {
+        navbarItem[navbarIndex].classList.add('animate__bounce');
+    })
+    navbarItem[navbarIndex].addEventListener("animationend", () => {
+        navbarItem[navbarIndex].classList.remove('animate__bounce');
+    })
+}
+
+/* Order button animation*/
+const orderBtn = document.querySelector('.order-btn');
+
+orderBtn.addEventListener("mouseover", () => {
+    orderBtn.classList.add('animate__pulse');
+})
+orderBtn.addEventListener('animationend', () => {
+    orderBtn.classList.remove('animate__pulse');
+});
+
+/* View more, read more, all photos, price animations*/ 
+const formBtn = document.querySelector('.form-btn');
+
+orderBtn.addEventListener("mouseover", () => {
+    formBtn.classList.add('animate__pulse');
+})
+orderBtn.addEventListener('animationend', () => {
+    formBtn.classList.remove('animate__pulse');
+});
--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,10 @@
 const dots = document.querySelectorAll('.dot');
 const imgDiv = document.querySelectorAll('.slide-div')
 
-for (let dotIndex = 0; dotIndex < dots.length; dotIndex++) {
-    if (!dots[dotIndex].classList.contains('active-dot')) {
-        dots[dotIndex].addEventListener("click", () => {
-            activeSlideHandler(dotIndex);
-        })
-    }
-}
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("hello, world")
+    dots[0].classList.add('active-dot')
+})
 
 const activeSlideHandler = (dotIndex) => {
     for (let divIndex = 0; divIndex < imgDiv.length; divIndex++) {
@@ -18,4 +15,12 @@ const activeSlideHandler = (dotIndex) => {
     imgDiv[dotIndex].classList.add('animate__jackInTheBox');
     imgDiv[dotIndex].style.setProperty('--animate-duration', '1.7s');
     dots[dotIndex].classList.add('active-dot');
+}
+
+for (let dotIndex = 0; dotIndex < dots.length; dotIndex++) {
+    if (!dots[dotIndex].classList.contains('active-dot')) {
+        dots[dotIndex].addEventListener("click", () => {
+            activeSlideHandler(dotIndex);
+        })
+    }
 }
