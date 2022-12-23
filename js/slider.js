24a84ea8e6a31019a046960d98b49ac319177d8e
Aleksandr <alex.avramenko2004@gmail.com>
2022-12-23 16:40:50 -0500

    Changed animation of the slider

--- js/slider.js
+++ js/slider.js
@@ -2,20 +2,8 @@ const dots = document.querySelectorAll('.dot');
 const imgDiv = document.querySelectorAll('.slide-div')
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("hello, world")
     dots[0].classList.add('active-dot')
 })
-
-const activeSlideHandler = (dotIndex) => {
-    for (let divIndex = 0; divIndex < imgDiv.length; divIndex++) {
-        imgDiv[divIndex].classList.add('invisible');
-        dots[divIndex].classList.remove('active-dot');
-    }
-    imgDiv[dotIndex].classList.remove('invisible');
-    imgDiv[dotIndex].classList.add('animate__jackInTheBox');
-    imgDiv[dotIndex].style.setProperty('--animate-duration', '1.7s');
-    dots[dotIndex].classList.add('active-dot');
-}
 
 for (let dotIndex = 0; dotIndex < dots.length; dotIndex++) {
     if (!dots[dotIndex].classList.contains('active-dot')) {
@@ -23,4 +11,15 @@ for (let dotIndex = 0; dotIndex < dots.length; dotIndex++) {
             activeSlideHandler(dotIndex);
         })
     }
+}
+
+const activeSlideHandler = (dotIndex) => {
+    for (let divIndex = 0; divIndex < imgDiv.length; divIndex++) {
+        imgDiv[divIndex].classList.add('invisible');
+        dots[divIndex].classList.remove('active-dot');
+    }
+    imgDiv[dotIndex].classList.remove('invisible');
+    imgDiv[dotIndex].classList.add('animate__slideInRight');
+    imgDiv[dotIndex].style.setProperty('--animate-duration', '1.7s');
+    dots[dotIndex].classList.add('active-dot');
 }
