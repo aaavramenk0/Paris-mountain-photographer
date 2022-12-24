b8d44dda80734495a266903fb86db4677aa3a1f1
Aleksandr <alex.avramenko2004@gmail.com>
2022-12-24 11:32:09 -0500

    Added scroll animation to the slider

--- js/slider.js
+++ js/slider.js
@@ -26,10 +26,17 @@ const activeSlideHandler = (dotIndex) => {
 }
 */
 
+
+
 const slideDiv = document.querySelector('.slide-div')
 const prevArrow = document.querySelector('.prev')
 const nextArrow = document.querySelector('.next')
 const dots = document.querySelectorAll('.dot');
+const imgs = document.querySelectorAll('.slider-photo')
+
+document.addEventListener("DOMContentLoaded", () => {
+    dots[0].classList.add('active-dot')
+})
 
 /* Scroll animation for arrows */
 prevArrow.addEventListener("click", () => {
@@ -40,3 +47,38 @@ nextArrow.addEventListener("click", () => {
 })
 
 /* Scroll animation for dots */
+slideDiv.addEventListener('change', () => {
+    for(let dotIndex = 0; dotIndex < dots.length; dotIndex++) {
+        for(let imgIndex = 0; imgIndex < imgs.length; imgIndex++) {
+            if(imgs[imgIndex].clientLeft == 370) {
+                console.log(imgs[imgIndex])
+            }
+        }
+    }
+})
+
+/* Draggable scroll for slider */
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slideDiv.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - slideDiv.offsetLeft;
+  slideDiv.style.cursor = "grabbing";
+  scrollLeft = slideDiv.scrollLeft;
+});
+slideDiv.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+slideDiv.addEventListener('mouseup', () => {
+  isDown = false;
+  slideDiv.style.cursor = "grab";
+});
+slideDiv.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slideDiv.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slideDiv.scrollLeft = scrollLeft - walk;
+});
