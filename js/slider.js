656caab1498eecdfa78999b1a74196be2e007337
Aleksandr <alex.avramenko2004@gmail.com>
2022-12-28 15:46:59 -0500

    Changed slider dots

--- js/slider.js
+++ js/slider.js
@@ -2,18 +2,19 @@ const slideDiv = document.querySelector('.slide-div')
 const prevArrow = document.querySelector('.prev')
 const nextArrow = document.querySelector('.next')
 const imgs = document.querySelectorAll('.slider-photo')
-
+const dots = document.querySelectorAll('.dots')
 
 /* Scroll animation for arrows */
 prevArrow.addEventListener("click", () => {
-    slideDiv.scrollLeft -= 440;
+  slideDiv.scrollLeft -= imgs[0].offsetWidth + 70;
 })
 nextArrow.addEventListener("click", () => {
-    slideDiv.scrollLeft += 440;
+  slideDiv.scrollLeft += imgs[0].offsetWidth + 70;
 })
 
+
 /* Draggable scroll for slider */
-let isDown = false;
+/* let isDown = false;
 let startX;
 let scrollLeft;
 
@@ -22,6 +23,7 @@ slideDiv.addEventListener('mousedown', (e) => {
   startX = e.pageX - slideDiv.offsetLeft;
   slideDiv.style.cursor = "grabbing";
   scrollLeft = slideDiv.scrollLeft;
+
 });
 slideDiv.addEventListener('mouseleave', () => {
   isDown = false;
@@ -38,4 +40,6 @@ slideDiv.addEventListener('mousemove', (e) => {
   let x = e.pageX - slideDiv.offsetLeft;
   let walk = (x - startX) * 3; //scroll-speed   
   slideDiv.scrollLeft = scrollLeft - walk;
-});
+}); */
+
+/* Dots navigation */
