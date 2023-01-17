8b6412e6e19ec1423ba18c72fd0a15963938c54b
Aleksandr <alex.avramenko2004@gmail.com>
2023-01-17 08:21:55 -0500

    Slider done

--- js/slider.js
+++ js/slider.js
@@ -3,13 +3,46 @@ const prevArrow = document.querySelector('.prev')
 const nextArrow = document.querySelector('.next')
 const imgs = document.querySelectorAll('.slider-photo')
 const dots = document.querySelectorAll('.dots')
+let k = 0;
 
 /* Scroll animation for arrows */
 prevArrow.addEventListener("click", () => {
-  slideDiv.scrollLeft -= imgs[0].offsetWidth + 70;
+  if (slideDiv.scrollLeft != 0) {
+    k--;
+    slideDiv.scrollLeft = k * 440;
+    dots.forEach(dot => {
+      dot.classList.remove('active-dot')
+    });
+    if (k > 0) {
+      dots[k].classList.add('active-dot')
+    } else {
+      dots[0].classList.add('active-dot')
+    }
+  } else {
+    slideDiv.scrollLeft = 7480;
+    dots.forEach(dot => {
+      dot.classList.remove('active-dot')
+    });
+    dots[17].classList.add('active-dot')
+    k = 17;
+  }
 })
 nextArrow.addEventListener("click", () => {
-  slideDiv.scrollLeft += imgs[0].offsetWidth + 70;
+  if (slideDiv.scrollLeft <= 16 * 440) {
+    k++;
+    slideDiv.scrollLeft = k * 440;
+    dots.forEach(dot => {
+      dot.classList.remove('active-dot')
+    });
+    dots[k].classList.add('active-dot')
+  } else {
+    slideDiv.scrollLeft = 0;
+    dots.forEach(dot => {
+      dot.classList.remove('active-dot')
+    });
+    dots[0].classList.add('active-dot')
+    k = 0;
+  }
 })
 
 /* Scroll animation for dots */
@@ -20,5 +53,6 @@ for (let dotIndex = 0; dotIndex < dots.length; dotIndex++) {
     });  
       slideDiv.scrollLeft = dotIndex * 440;
       dots[dotIndex].classList.add('active-dot')
+      k = dotIndex;
   })
 }
