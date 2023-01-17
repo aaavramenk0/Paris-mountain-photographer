c632e9e74199c1b4bccef1aa51018d98364e997a
Aleksandr <alex.avramenko2004@gmail.com>
2023-01-17 07:39:25 -0500

    Slider updates

--- js/slider.js
+++ js/slider.js
@@ -12,34 +12,13 @@ nextArrow.addEventListener("click", () => {
   slideDiv.scrollLeft += imgs[0].offsetWidth + 70;
 })
 
-
-/* Draggable scroll for slider */
-/* let isDown = false;
-let startX;
-let scrollLeft;
-
-slideDiv.addEventListener('mousedown', (e) => {
-  isDown = true;
-  startX = e.pageX - slideDiv.offsetLeft;
-  slideDiv.style.cursor = "grabbing";
-  scrollLeft = slideDiv.scrollLeft;
-
-});
-slideDiv.addEventListener('mouseleave', () => {
-  isDown = false;
-});
-slideDiv.addEventListener('mouseup', () => {
-  slideDiv.style.cursor = "grab";
-  isDown = false;
-});
-slideDiv.addEventListener('mousemove', (e) => {
-  if(!isDown) {
-    return
-  }
-  e.preventDefault();
-  let x = e.pageX - slideDiv.offsetLeft;
-  let walk = (x - startX) * 3; //scroll-speed   
-  slideDiv.scrollLeft = scrollLeft - walk;
-}); */
-
-/* Dots navigation */
+/* Scroll animation for dots */
+for (let dotIndex = 0; dotIndex < dots.length; dotIndex++) {
+  dots[dotIndex].addEventListener("click", () => {
+    dots.forEach(dot => {
+      dot.classList.remove('active-dot')
+    });  
+      slideDiv.scrollLeft = dotIndex * 440;
+      dots[dotIndex].classList.add('active-dot')
+  })
+}
